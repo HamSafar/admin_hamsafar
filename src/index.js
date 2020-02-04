6131c0c0398e2ee1e9a5354454981a5041260786
Hayyan Hami <43971080+hayyaun@users.noreply.github.com>
2020-02-04 11:41:56 +0330

    new packages

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 import promiseMiddleware from 'redux-promise'
+import axios from 'axios'
 
 import reducers from './reducers'
 import App from './App/App'
 
 import './index.scss'
 
-import axios from 'axios'
+
 const defaultURL = "http://2.184.239.248:9090/"
 axios.defaults.baseURL = defaultURL
 axios.defaults.headers.post['Content-Type'] ='application/json;charset=utf-8';
